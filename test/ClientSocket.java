79f40508257181841ddc7e3a116ec73617470647
Laptop <Laptop@130.203.80.240>
2019-04-05 14:59:17 -0400

    Merge origin/master
    Conflicts:
    	src/userinterface/Client.java
    	src/userinterface/MasterServer.java

---
+++ test/ClientSocket.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Laptop
+ */
+import java.net.*;
+import java.io.*;
+
+public class ClientSocket {
+    protected static Socket socket;
+    private static int newPort = 11000;
+    protected InputStream iStream;            // Instance variables
+    protected OutputStream oStream;
+    
+    public ClientSocket(String url, int port)  {
+        try {
+             socket = new Socket(url, port);
+             System.out.println(port);
+             //newPort = Integer.parseInt(readFromSocket(socket));
+             //socket = new Socket(url, newPort);
+             System.out.println("CLIENT: connected to " + url + ":" + port);
+             System.out.println("CLIENT: port number " + socket.getLocalPort());
+             //newPort = newPort + 1;
+             
+         } catch (Exception e) {
+             e.printStackTrace();
+             System.exit(1);
+         }
+     } //ClientSocket
+    public void start() {
+         try {
+             requestService(socket);  
+             socket.close();
+             System.out.println("CLIENT: connection closed");
+             
+         } catch (IOException e) { 
+             System.out.println(e.getMessage());
+         }
+         
+     } // run()
+protected void requestService(Socket socket) throws IOException {         
+        String servStr = readFromSocket(socket);          // Check for "Hello"
+        System.out.println("SERVER: " + servStr);         // Report the server's response
+        if (servStr.substring(0,5).equals("Hello")) {
+            //System.out.println("CLIENT: type a line or 'goodbye' to quit"); // Prompt the user
+            String userStr = "";
+            do {
+                userStr = readFromSocket(socket);                   // Get input from socket
+                writeToSocket(socket, userStr + "\n");          // Send it to server
+                servStr = readFromSocket(socket);               // Read the server's response
+                System.out.println("SERVER: " + servStr);       // Report the server's response
+            } while (!userStr.toLowerCase().equals("goodbye")); // Until user says 'goodbye'
+        }
+        
+    } // requestService()
+    protected String readFromSocket(Socket sock) throws IOException {
+        iStream = sock.getInputStream();
+        String str="";
+        char c;
+        while (  ( c = (char) iStream.read() ) != '\n') 
+            str = str + c + "";
+        return str;
+        }
+
+    /**
+     *  writeToSocket() reads a String to a given socket
+     *  @param sock -- a reference to the Socket object
+     *  @param str -- the string to be written
+     * @throws java.io.IOException
+     */
+    protected void writeToSocket(Socket sock, String str) throws IOException {
+        oStream = sock.getOutputStream();
+        if (str.charAt( str.length() - 1 ) != '\n')
+            str = str + '\n';
+        for (int k = 0; k < str.length() ; k++)
+            oStream.write(str.charAt(k));
+    } // writeToSocket()
+public static void main (String args[]) {
+        ClientSocket client = new ClientSocket("localhost", 10001);
+        client.start();
+    } // main()
+    
+}
+
+
