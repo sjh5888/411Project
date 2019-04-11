6d0e32dbfa76548831b5ea2e2a50d4d0d21f4f61
gkemp <gkemp@GKLaptop>
2019-04-11 14:41:12 -0400

    test

--- src/java/data/AccessDb.java
+++ src/java/data/AccessDb.java
@@ -5,6 +5,7 @@
  */
 package data;
 
+import controllers.ProductsBean;
 import java.sql.*;
 import java.io.*;
 import java.util.*;
@@ -77,7 +78,14 @@ public class AccessDb {
         return resultStr;
     }
     
-    public static Product indProductQuery(String queryString, boolean type) throws SQLException {
+    /**
+     * 
+     * @param queryString
+     * @param type
+     * @return
+     * @throws SQLException 
+     */
+    public static ProductsBean indProductQuery(String queryString, boolean type) throws SQLException {
 
         try {
             ResourceBundle resources;
@@ -120,14 +128,106 @@ public class AccessDb {
             }
             while (rs.next()) {
                 for (int x = 1; x <= columnCount; x++) {
-                    Product product = new Product();
+                    ProductsBean product = new ProductsBean();
                     if (columnName.equals("PRODUCTID")){
-                        product.setProdID(rs.getString());
+                        product.setProductId(rs.getString());
                     }
-
+                    else if (columnName.equals("NAME")){
+                        product.setName(rs.getString());
+                    }
+                    else if (columnName.equals("CATEGORYID")){
+                        product.setCategoryId(rs.getString());
+                    }
+                    else if (columnName.equals("PRICE")){
+                        product.setPrice(rs.getString());
+                    }
+                    else if (columnName.equals("DESCRIPTION")){
+                        product.setDescription(rs.getString());
+                    }
+                    else if (columnName.equals("INVENTORY")){
+                        product.setInventory(rs.getString());
+                    }
+                    else if (columnName.equals("SHIPPINGLOCATION")){
+                        product.setShippingLocation(rs.getString());
+                    }
+                    else if (columnName.equals("WEIGHT")){
+                        product.setWeight(rs.getString());
+                    }
+                    else if (columnName.equals("SIZE")){
+                        product.setSize(rs.getString());
+                    }
+                    else if (columnName.equals("IMAGELOCATION")){
+                        product.setImageLocation(rs.getString());
+                    }
+            
+                    
+                    return product;
                 }
+            }
+                
+    /**
+     * Return array of ProductBeans
+     * @param queryString
+     * @param type
+     * @return
+     * @throws SQLException 
+     */
+    public static ProductsBean arrayProductQuery(String queryString, boolean type) throws SQLException {
+        
+         try {
+            ResourceBundle resources;
+            InputStream in = null;
+
+            in = ClassLoader.getSystemResourceAsStream("db.properties");
+
+            resources = new PropertyResourceBundle(in);
+
+            in.close();
+
+            className = resources.getString("jdbc.driver");
+            url = resources.getString("jdbc.url");
+            //System.out.println(url);
+            user = resources.getString("jdbc.user");
+            password = resources.getString("jdbc.password");
+        } catch (Exception exp) {
+            System.out.println("Couldn't load resources." + exp);
+            System.exit(-1);
+        }
+
+        try {
+            Class.forName(className);
+        } catch (Exception e) {
+            System.out.println("Failed to load  driver.");
+
+        }
+
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(queryString);
+
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+
+            for (int x = 1; x <= columnCount; x++) {
+                String columnName = rsmd.getColumnName(x);
 
             }
+            while (rs.next()) {
+                for (int x = 1; x <= columnCount; x++) {
+                    ProductsBean products = new ProductsBean();
+                    if (columnName.equals("PRODUCTID")){
+                        products.setProductId(rs.getString());
+                    }
+                    else if (columnName.equals("NAME")){
+                        products.setName(rs.getString());
+                    }
+                    else if (columnName.equals("PRICE")){
+                        products.setPrice(rs.getString());
+                    }
+                }   
+            }
+    
             stmt.close();
             con.close();
         } catch (Exception e) {
@@ -136,8 +236,4 @@ public class AccessDb {
         return resultStr;
     }
     
-    /**
-     * Need 3 more methods that returns 1 ProductBean, 1 array of ProductBeans,
-     * and 1 String (Description)
-     */
-}
+    
