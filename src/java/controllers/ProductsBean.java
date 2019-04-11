713b91629eb8ad150806814933fdf0cad422c52e
elgoo <elgoo@130.203.102.200>
2019-04-11 14:06:53 -0400

    testing

--- src/java/controllers/ProductsBean.java
+++ src/java/controllers/ProductsBean.java
@@ -10,5 +10,39 @@ package controllers;
  * @author elgoo
  */
 public class ProductsBean {
+    private long productId = 0;
+    private String name = null;
+    private long categoryId = 0;
+    private double price = 0;
+    private String description = null;
+    private int inventory = 0;
+    private String shippingLocation = null;
+    private double weight = 0;
+    private String size = null;
+    private String imageLocation = null;
+    
+ public ProductsBean(){
+     
+ }
+    public void setProductId(long pId){
+        productId = pId;
+    }
+    
+     public void setName(String n){
+        name = n;
+    }
+     
+     public void setCategoryId(long cId){
+         categoryId = cId;
+     }
+     
+    // publoc void setPrice(p)
+    
+    public long getProductId(){
+        return productId;
+    }
+    
+   
+    
     
 }
