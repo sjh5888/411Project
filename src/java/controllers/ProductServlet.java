5c9a0ea1b905d2bd16a4de36bce14a6c9c349985
roscoeturner_ <ginganinjarules@gmail.com>
2019-04-18 23:00:50 -0400

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

--- src/java/controllers/ProductServlet.java
+++ src/java/controllers/ProductServlet.java
@@ -96,7 +96,7 @@ public class ProductServlet extends HttpServlet {
             try {
                 products = arrayProductQuery(query, true); // this should return an array of product beans I believe.
                 
-                System.out.println(products[0].getProductId());
+                System.out.println("Prod ID of Element 1: " + products[0].getProductId());
                 
                 request.setAttribute("products", products); //products should be accessible to the view using the "products" attribute
             } catch (SQLException ex) {
